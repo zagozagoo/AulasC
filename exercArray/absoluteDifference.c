8751073117fa163da784d42a7434352914b49283
Zago! <vivizago4884@gmail.com>
2023-09-28 10:54:11 -0300

    Create absoluteDifference.c

---
+++ exercArray/absoluteDifference.c
@@ -0,0 +1,12 @@
+#include "arrFunctions.h"
+//Faça um programa que receba dois arrays e calcule a diferença absoluta deles.
+
+ int main()
+ {
+    int arr1[4];
+    int arr2[4];
+
+    receiveValues(arr1, arr2);
+
+    subValues(arr1, arr2);
+ }
