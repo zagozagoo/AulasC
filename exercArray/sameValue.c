6ce7e1f47828dbc946c3fb640e0ac74fa27ee526
Zago! <vivizago4884@gmail.com>
2023-10-03 10:53:14 -0300

    Update sameValue.c

--- exercArray/sameValue.c
+++ exercArray/sameValue.c
@@ -1,2 +1,20 @@
 // Leia um array com 10 posições e identifique se existem valores iguais neste array
 //se existirem, envie para um outro array e o exiba na tela.
+#include <stdio.h>
+
+void receiveValues(int* arr1)
+{
+    for (int i = 0; i < 10; i++)
+    {
+            printf("\nFirst array value in position %d \n", i);
+            scanf("%i", &arr1[i]);
+    }
+}
+
+int main()
+{
+    int array[10];
+    receiveValues(array);
+
+    printf("\nArray values: ");
+}
