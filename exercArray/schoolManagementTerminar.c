41c389a2c8abeb0581e3d5f5558dfdc86e255b05
Zago! <vivizago4884@gmail.com>
2023-10-04 11:09:13 -0300

    Create schoolManagementTerminar.c

---
+++ exercArray/schoolManagementTerminar.c
@@ -0,0 +1,2 @@
+//Faça um programa para uma escola chamada IANES que fará todo o gerenciamento de alunos, cursos, e média de alunos
+//Utilize um arquivo de texto para fazer esse gerenciamento.(Escrita e leitura)
