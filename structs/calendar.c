1fb760414e356e9e41c639aa951d7ddea8bcee12
Zago! <vivizago4884@gmail.com>
2023-10-10 09:01:05 -0300

    Update calendar.c

---
+++ structs/calendar.c
@@ -0,0 +1,3 @@
+//1. Escreva um trecho de codigo para fazer a criação dos novos tipos de dados conforme solicitado abaixo:
+//• Horário: composto de hora, minutos e segundos. • Data: composto de dia, mês e ano.
+//• Compromisso: composto de uma data, horario e texto que descreve o compromisso.
