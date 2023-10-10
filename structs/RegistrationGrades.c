e12ed3eeee0714c67034772134660152d4d9a990
Zago! <vivizago4884@gmail.com>
2023-10-10 09:01:03 -0300

    Update RegistrationGrades.c

---
+++ structs/RegistrationGrades.c
@@ -0,0 +1,8 @@
+//3.Crie uma estrutura representando os alunos de um determinado curso. A estrutura deve conter
+//a matrícula do aluno, nome, nota da primeira, segunda e terceira prova.
+
+//(a) Permita ao usuario entrar com os dados de 5 alunos.
+//(b) Encontre o aluno com maior nota da primeira prova.
+//(c) Encontre o aluno com maior media geral.
+//(d) Encontre o aluno com menor media geral
+//(e) Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para aprovacao
