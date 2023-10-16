8779a6d7e8a31eb4c2268f01f449b1992fc01576
Zago! <vivizago4884@gmail.com>
2023-10-16 10:44:29 -0300

    Update RegistrationGrades.c

--- structs/RegistrationGrades.c
+++ structs/RegistrationGrades.c
@@ -6,3 +6,51 @@
 //(c) Encontre o aluno com maior media geral.
 //(d) Encontre o aluno com menor media geral
 //(e) Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para aprovacao
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
+{
+    int matricula;
+    char nome[50];
+    int primeira_nota;
+    int segunda_nota;
+    int terceira_nota;
+
+} Aluno;
+
+int main()
+{
+    Aluno alunos[5];
+    int maiorP1, media;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nDigite numero de matricula, nome, e notas da primeria, segunda e terceira prova, respectivamente\n");
+        scanf(" %i %s %i %i %i", &alunos[i].matricula, &alunos[i].nome, &alunos[i].primeira_nota, &alunos[i].segunda_nota, &alunos[i].terceira_nota);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        if(alunos[i].primeira_nota > alunos[i + 1].primeira_nota)
+        {
+            maiorP1 = i;
+        }
+    }
+
+    
+
+    printf("\n___Aprovaçoes___\n");
+    if (media >= 6)
+    {
+        prinft("\nAluno: %s = Aprovado!\n", alunos[i].nome);
+    }
+    else
+    {
+        prinft("\nAluno: %s = Reprovado\n", alunos[i].nome);
+    }
+    
+
+    return 0;
+}
