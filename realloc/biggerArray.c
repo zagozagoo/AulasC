fe3e085604e2afc614d66080e65606a7eacf7834
Zago! <vivizago4884@gmail.com>
2023-10-16 10:44:38 -0300

    Create biggerArray.c

---
+++ realloc/biggerArray.c
@@ -0,0 +1,2 @@
+//Crie um programa que peça ao usuário para inserir elementos em um array dinamicamente redimensionado usando realloc.
+//O array deve crescer à medida que o usuário insere mais elementos.
