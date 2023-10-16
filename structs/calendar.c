c23669ddd3e3f07aea138066fd4fa40591899cf6
Zago! <vivizago4884@gmail.com>
2023-10-16 09:01:15 -0300

    Update calendar.c

--- structs/calendar.c
+++ structs/calendar.c
@@ -1,3 +1,46 @@
 //1. Escreva um trecho de codigo para fazer a criação dos novos tipos de dados conforme solicitado abaixo:
 //• Horário: composto de hora, minutos e segundos. • Data: composto de dia, mês e ano.
 //• Compromisso: composto de uma data, horario e texto que descreve o compromisso.
+
+#include <stdio.h>
+#include <string.h>
+
+    typedef struct
+    {
+        int hora;
+        int minuto;
+        int segundo;
+    }Horario;
+
+    typedef struct 
+    {
+        int dia;
+        int mes;
+        int ano;
+    }Data;
+
+    typedef struct 
+    {
+        Data data1;
+        Horario horario1;
+        char descricao[60];
+    }Compromisso;
+
+int main()
+{
+    Compromisso compromisso;
+
+    printf("\nInsira respectivamente: DIA, MES, ANO, HORA e MINUTO para o compromisso:\n");
+    scanf("\n%i %i %i %i %i\n", &compromisso.data1.dia, &compromisso.data1.mes, &compromisso.data1.ano, &compromisso.horario1.hora, &compromisso.horario1.minuto);
+    printf("Digite a descricao do compromisso:\n");
+    scanf("\n%s\n", &compromisso.descricao);
+    
+    printf("\n_____Seu evento foi adicionado!_____\n");
+    printf("\n%i / %i / %i \n%i : %i\n", compromisso.data1.dia, compromisso.data1.mes, compromisso.data1.ano, compromisso.horario1.hora, compromisso.horario1.minuto);
+    scanf("\n%s\n", compromisso.descricao);
+
+    return 0;
+}
+    
+    
+    
